d9d96a36d8fcc9c101aab18fd265ce2dad21f096
liviasousas <sousalivia119@gmail.com>
2022-11-13 04:09:03 -0300

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+    
+    let imagem = document.querySelector(".starbucks")
+    let circulo = document.querySelector(".circulo")
+    function trocaImagem(endereco){
+        imagem.src = endereco
+
+    }
+
+    function trocaCor(cor) {
+    circulo.style.background = cor
+}
+</script>
